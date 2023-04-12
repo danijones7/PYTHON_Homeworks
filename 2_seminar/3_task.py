2966424a3827cbc97d47cd385896aa46143d32c3
Jessie <jessica.v.rid@gmail.com>
2023-04-12 23:39:36 +0300

    Homework 2 added

---
+++ 2_seminar/3_task.py
@@ -0,0 +1,15 @@
+# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2^k), не превосходящие числа N.
+
+# Пример:
+# Ввод: 13 -> 1, 2, 4, 8
+
+N = int (input("Введите число N: "))
+result = 0 
+count = 0
+while result <= N:
+    result = 2 ** count
+    if result <= N:
+        count = count + 1
+    else:
+        break
+    print (result)
